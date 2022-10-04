45de08776d819b3773aeac595cb0d3afc2e56256
rahul chaube <rahul.chaube@hpe.com>
2022-10-04 20:23:18 +0530

    Updated Task

---
+++ svc-task/tcommon/common.go
@@ -0,0 +1,24 @@
+package tcommon
+
+import (
+	"fmt"
+
+	"github.com/ODIM-Project/ODIM/lib-utilities/common"
+	"github.com/ODIM-Project/ODIM/lib-utilities/config"
+	l "github.com/ODIM-Project/ODIM/lib-utilities/logs"
+)
+
+var (
+	// ConfigFilePath holds the value of odim config file path
+	ConfigFilePath string
+)
+
+func TrackConfigFileChanges() {
+	eventChan := make(chan interface{})
+	go common.TrackConfigFileChanges(ConfigFilePath, eventChan)
+	for {
+		l.Log.Info(<-eventChan) // new data arrives through eventChan channel
+		fmt.Println("File change received ********* ")
+		l.Log.Logger.Level = config.Data.LogLevel
+	}
+}
