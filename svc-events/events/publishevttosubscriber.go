255de7b3270ee50ffe946289192a4ec17d9ca8d9
rahul chaube <rahul.chaube@hpe.com>
2023-01-04 14:40:50 +0530

    Update 2

--- svc-events/events/publishevttosubscriber.go
+++ svc-events/events/publishevttosubscriber.go
@@ -861,6 +861,7 @@ func getSubscriptions(originOfCondition, systemId, hostIp string) (subs []evmode
 			if isValidAggregateId {
 				for subId := range subscriptions {
 					sub, isValidSubId := getSubscriptionDetails(subId)
+					sub.OriginResources = append(sub.OriginResources, originOfCondition)
 					if isValidSubId {
 						subs = append(subs, sub)
 					}
